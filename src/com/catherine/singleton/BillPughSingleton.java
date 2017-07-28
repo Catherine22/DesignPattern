19e6dd5dace8d7d81593e1d21c946f552cec038c
Catherine <Catherine22@users.noreply.github.com>
2017-07-28 11:18:56 +0800

    Update BillPughSingleton.java

--- src/com/catherine/singleton/BillPughSingleton.java
+++ src/com/catherine/singleton/BillPughSingleton.java
@@ -12,6 +12,8 @@ package com.catherine.singleton;
  * 内部静态类
  * 特点是加载类时比较快，但运行时获取对象的速度比较慢，线程安全
  * 只要应用中不使用内部类 JVM 就不会去加载这个单例类，也就不会创建单例对象，从而实现懒汉式的延迟加载和线程安全。
+ * 加载BillPughSingleton时，并没有加载SingletonHolder，这就是为啥能延迟加载。
+ * 为什么是线程安全是因为虚拟机有对类加载做处理，确保类加载一定是线程安全。
  */
 public class BillPughSingleton {
     private BillPughSingleton() {
